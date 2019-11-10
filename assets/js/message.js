aa1c6f11e929e7a70c8538c71ca9a97cf6ff55af
Mamoru Hashimoto <you@example.com>
2019-11-10 16:55:31 +0900

    modified assets/js/message.js

--- assets/js/message.js
+++ assets/js/message.js
@@ -1,29 +1,67 @@
-var sock = new WebSocket('wss://localhost.localdomain:8080/ws');
+var sock = null;
+var uri  = 'wss://localhost.localdomain:8080/ws';
+
+var open = function() {
+  if(sock && sock.readState !== 1) {
+    connect();
+  }
+};
+
+var close = function() {
+  if(sock && sock.readyState === 1) {
+    disconnect();
+  }
+};
 
 var send = function(msgid, body) {
   var packet = { 'id': msgid, 'body': body };
   var json = JSON.stringify(packet)
-  sock.send(json)
+  message(json);
 };
 
-sock.onopen = function(e) {
-  $("#output").append((new Date()) + " ===> " + "Connect success.\n")
-  $("#send").click(function(e) {
-    var msg = "PING";
-    $("#output").append((new Date()) + " ===> " + msg + "\n")
-    send(1, { 'msg': msg });
-  });
+var message = function(json) {
+  sock.send(json);
 };
 
-sock.onclose = function(e) {
-  $("#output").append((new Date()) + " <=== " + "Connect close.\n")
+var disconnect = function() {
+  sock.close();
 };
 
-sock.onmessage = function(e) {
-  var json = JSON.parse(e.data)
-  var msgid = json.id;
-  var body = json.body;
-  if (msgid == 1) {
-    $("#output").append((new Date()) + " <=== " + body.msg + "\n");
-  }
+var connect = function() {
+  sock = new WebSocket(uri);
+
+  sock.onopen = function(e) {
+    $("#output").append((new Date()) + " ===> " + "Connect success.\n")
+
+    $('#open').off();
+    $("#close").click(function(e) {
+      close();
+    });
+    $("#send").click(function(e) {
+      var msg = "PING";
+      $("#output").append((new Date()) + " ===> " + msg + "\n")
+      send(1, { 'msg': msg });
+    });
+  };
+
+  sock.onclose = function(e) {
+    $("#output").append((new Date()) + " <=== " + "Connect close.\n")
+
+    $('#send').off();
+    $('#close').off();
+    $("#open").click(function(e) {
+      open();
+    });
+  };
+
+  sock.onmessage = function(e) {
+    var json = JSON.parse(e.data)
+    var msgid = json.id;
+    var body = json.body;
+    if (msgid == 1) {
+      $("#output").append((new Date()) + " <=== " + body.msg + "\n");
+    }
+  };
 };
+
+connect();
